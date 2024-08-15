6087f3975633f3f835b4c53d4368bd6bf5f41c66
nikeshwebappFresher <nikeshwebapp@gmail.com>
2024-08-15 12:57:45 +0530

    freshers page done

---
+++ freshers/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.getElementById("popup").style.display = "block";
+}
+
+document.querySelector(".close").onclick = function() {
+    document.getElementById("popup").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById("popup")) {
+        document.getElementById("popup").style.display = "none";
+    }
+}
